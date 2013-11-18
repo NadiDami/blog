7bba3d932b46f6b3742f51591353d94aedb13ab8
Nadia Odunayo <nodunayo@gmail.com>
2013-11-18 17:29:01 +0000

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '0737846620c12bb5162391bd5fec66ced1143a26abb844785814f42ff526ed144c81af9a24dd97db9e6f56227a712d235f09c533a65821ff45c2386073f83977'
---
+++ spec/features/post_feature_spec.rb
@@ -0,0 +1,17 @@
+
+
+def create_a_post
+  Post.create(title: 'Hello world', body: 'Helllooooo')
+end
+
+
+
+describe 'blog posts index page' do
+
+  it 'should display a list of posts' do
+    create_a_post
+    visit '/posts'
+    expect(page).to have_content "Hello world"
+  end
+
+end
